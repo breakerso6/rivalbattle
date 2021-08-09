62b58f9f33b88444a1ae1cbee4007173815b1a07
breakerso6 <88638620+breakerso6@users.noreply.github.com>
2021-08-09 14:02:51 +0900

    Update battle.js

--- battle.js
+++ battle.js
@@ -1,8 +1,13 @@
 let characount = 0;
 
-//クリーチャーシャッフル
 var creature = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-var a = creature.length;
+
+for (var i = creature.length - 1; i > 0; i--) {
+    var r = Math.floor(Math.random() * (i + 1));
+    var tmp = creature[i];
+    creature[i] = creature[r];
+    creature[r] = tmp;
+}
 
 var creatureimg = ["images/rival/enemycreature/01.jpg",
     "images/rival/enemycreature/02.jpg",
@@ -67,34 +72,34 @@ function call() {
 
     switch (newcre) {
         case 1:
-            mark.insertAdjacentHTML('beforebegin', '<div class="creature" id="01cre">< img src = "images/rival/enemycreature/01.jpg" id = "01img" class= "untap" ><br><button onclick="tap(0)">タップ/アンタップ</button><button onclick="burn(0)">除去</button></div>');
+            mark.insertAdjacentHTML('beforebegin', '<span class="creature" id="01cre"><img src="images/rival/enemycreature/01.jpg" id="01img" class="untap"><br><button onclick="tap(0)">tap/untap</button><button onclick="burn(0)">destroy</button></span>');
             break;
         case 2:
-            mark.insertAdjacentHTML('beforebegin', '<div class="creature" id="02cre">< img src = "images/rival/enemycreature/02.jpg" id = "02img" class= "untap" ><br><button onclick="tap(1)">タップ/アンタップ</button><button onclick="burn(1)">除去</button></div>');
+            mark.insertAdjacentHTML('beforebegin', '<span class="creature" id="02cre"><img src="images/rival/enemycreature/02.jpg" id="02img" class="untap"><br><button onclick="tap(1)">tap/untap</button><button onclick="burn(1)">destroy</button></span>');
             break;
         case 3:
-            mark.insertAdjacentHTML('beforebegin', '<div class="creature" id="03cre">< img src = "images/rival/enemycreature/03.jpg" id = "03img" class= "untap" ><br><button onclick="tap(2)">タップ/アンタップ</button><button onclick="burn(2)">除去</button></div>');
+            mark.insertAdjacentHTML('beforebegin', '<span class="creature" id="03cre"><img src="images/rival/enemycreature/03.jpg" id="03img" class="untap"><br><button onclick="tap(2)">tap/untap</button><button onclick="burn(2)">destroy</button></span>');
             break;
         case 4:
-            mark.insertAdjacentHTML('beforebegin', '<div class="creature" id="04cre">< img src = "images/rival/enemycreature/04.jpg" id = "04img" class= "untap" ><br><button onclick="tap(3)">タップ/アンタップ</button><button onclick="burn(3)">除去</button></div>');
+            mark.insertAdjacentHTML('beforebegin', '<span class="creature" id="04cre"><img src="images/rival/enemycreature/04.jpg" id="04img" class="untap"><br><button onclick="tap(3)">tap/untap</button><button onclick="burn(3)">destroy</button></span>');
             break;
         case 5:
-            mark.insertAdjacentHTML('beforebegin', '<div class="creature" id="05cre">< img src = "images/rival/enemycreature/05.jpg" id = "05img" class= "untap" ><br><button onclick="tap(4)">タップ/アンタップ</button><button onclick="burn(4)">除去</button></div>');
+            mark.insertAdjacentHTML('beforebegin', '<span class="creature" id="05cre"><img src="images/rival/enemycreature/05.jpg" id="05img" class="untap"><br><button onclick="tap(4)">tap/untap</button><button onclick="burn(4)">destroy</button></span>');
             break;
         case 6:
-            mark.insertAdjacentHTML('beforebegin', '<div class="creature" id="06cre">< img src = "images/rival/enemycreature/06.jpg" id = "06img" class= "untap" ><br><button onclick="tap(5)">タップ/アンタップ</button><button onclick="burn(5)">除去</button></div>');
+            mark.insertAdjacentHTML('beforebegin', '<span class="creature" id="06cre"><img src="images/rival/enemycreature/06.jpg" id="06img" class="untap"><br><button onclick="tap(5)">tap/untap</button><button onclick="burn(5)">destroy</button></span>');
             break;
         case 7:
-            mark.insertAdjacentHTML('beforebegin', '<div class="creature" id="07cre">< img src = "images/rival/enemycreature/07.jpg" id = "07img" class= "untap" ><br><button onclick="tap(6)">タップ/アンタップ</button><button onclick="burn(6)">除去</button></div>');
+            mark.insertAdjacentHTML('beforebegin', '<span class="creature" id="07cre"><img src="images/rival/enemycreature/07.jpg" id="07img" class="untap"><br><button onclick="tap(6)">tap/untap</button><button onclick="burn(6)">destroy</button></span>');
             break;
         case 8:
-            mark.insertAdjacentHTML('beforebegin', '<div class="creature" id="08cre">< img src = "images/rival/enemycreature/08.jpg" id = "08img" class= "untap" ><br><button onclick="tap(7)">タップ/アンタップ</button><button onclick="burn(7)">除去</button></div>');
+            mark.insertAdjacentHTML('beforebegin', '<span class="creature" id="08cre"><img src="images/rival/enemycreature/08.jpg" id="08img" class="untap"><br><button onclick="tap(7)">tap/untap</button><button onclick="burn(7)">destroy</button></span>');
             break;
         case 9:
-            mark.insertAdjacentHTML('beforebegin', '<div class="creature" id="09cre">< img src = "images/rival/enemycreature/09.jpg" id = "09img" class= "untap" ><br><button onclick="tap(8)">タップ/アンタップ</button><button onclick="burn(8)">除去</button></div>');
+            mark.insertAdjacentHTML('beforebegin', '<span class="creature" id="09cre"><img src="images/rival/enemycreature/09.jpg" id="09img" class="untap"><br><button onclick="tap(8)">tap/untap</button><button onclick="burn(8)">destroy</button></span>');
             break;
         case 10:
-            mark.insertAdjacentHTML('beforebegin', '<div class="creature" id="10cre">< img src = "images/rival/enemycreature/10.jpg" id = "10img" class= "untap" ><br><button onclick="tap(9)">タップ/アンタップ</button><button onclick="burn(9)">除去</button></div>');
+            mark.insertAdjacentHTML('beforebegin', '<span class="creature" id="10cre"><img src="images/rival/enemycreature/10.jpg" id="10img" class="untap"><br><button onclick="tap(9)">tap/untap</button><button onclick="burn(9)">destroy</button></span>');
             break;
     }
 
